98bf22dec54a3efba91b998755ae67f1721e1cc4
Miguel Leonardo Ortega <migleor@gmail.com>
2023-11-02 20:25:55 -0500

    implementacion colors

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,15 +1,17 @@
 import { writeFileSync } from 'node:fs';
+import colors from 'colors';
 
 const createFileTable = async ( base = 1, list=false, limit=1 ) => {
     try {
         let salida='';
         let prompt='';
-        prompt += '===============================\n';
-        prompt += `         TABLA DEL: ${base}    \n`;
-        prompt += '===============================\n';
+        console.clear();
+        prompt += colors.yellow('===============================\n');
+        prompt += colors.yellow(`         TABLA DEL: ${base}    \n`);
+        prompt += colors.yellow('===============================\n');
         for(let a=1; a<=limit; a++){
             salida += `${base} X ${a} es: ${ base * a } \n`;
-            prompt += `${base} X ${a} es: ${ base * a } \n`;
+            prompt += `${colors.blue(base)} ${colors.red('x')} ${colors.cyan(a)} ${colors.red('=')} ${ colors.green.bold(base * a) } \n`;
         }
         if(list){
             console.log(prompt)
